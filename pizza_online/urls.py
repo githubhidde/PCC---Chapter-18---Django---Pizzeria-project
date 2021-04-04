5a7f651ce283d2e46f8875d358b08386867df842
Hidde Tieleman <h.tieleman87@outlook.com>
2021-04-04 21:26:28 +0200

    2021-04-04

--- pizza_online/urls.py
+++ pizza_online/urls.py
@@ -8,4 +8,6 @@ app_name = 'pizza_online'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # Page that shows all pizzas.
+    path('pizzas/', views.pizzas, name='pizzas'),
 ]
--- pizza_online/views.py
+++ pizza_online/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from .models import Pizza
+
 def index(request):
 	"""The homepage for Pizza Online."""
 	return render(request, 'pizza_online/index.html')
+
+def pizzas(request):
+	pizzas = Pizza.objects.order_by('date_added')
+	context = {'pizzas': pizzas}
+	return render(request, 'pizza_online/pizzas.html', context)
